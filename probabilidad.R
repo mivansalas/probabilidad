4436849edd8f1ab691ef04b1af51a37b3973bcd8
Mario Iván Salas Domínguez <41874458+mivansalas@users.noreply.github.com>
2021-04-14 15:25:33 -0500

    Scrip video 22

---
+++ probabilidad.R
@@ -0,0 +1,14 @@
+Omega=c(1,2,3,4,5,6,7,8,9,10)
+A=c(1,2,3,4,5)
+B=c(1,4,5)
+C=c(4,6,7,8)
+Omega
+A
+B
+C
+intersect(A,B)
+AuB<-union(A,B)
+class(AuB)
+setdiff(B,C)
+combinations(n = 3,r = 2,repeats.allowed =FALSE)
+permutations(n = 3,r = 3)
